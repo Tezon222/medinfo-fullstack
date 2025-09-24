a1abbd9b1322327425ab5b07708fe62803ec11f4
Ryan-Zayne <phoenixastro97@gmail.com>
2025-09-24 21:23:22 +0100

    more eslint fixes

--- apps/frontend/app/(dashboard)/doctor/profile/page.tsx
+++ apps/frontend/app/(dashboard)/doctor/profile/page.tsx
@@ -28,12 +28,12 @@ function ProfilePage() {
 					lg:bg-white lg:p-8"
 			>
 				<div
-					className="border-medinfo-primary-main relative -z-10 size-[108px] rounded-full
-						border-[1.4px] bg-gray-300 lg:size-[140px]"
+					className="relative -z-10 size-[108px] rounded-full border-[1.4px]
+						border-medinfo-primary-main bg-gray-300 lg:size-[140px]"
 				>
 					<div
-						className="border-medinfo-primary-main absolute right-0 top-[2px] flex size-[24px]
-							items-center justify-center rounded-full border-[1.4px] bg-white lg:size-[40px]"
+						className="absolute top-[2px] right-0 flex size-[24px] items-center
+							justify-center rounded-full border-[1.4px] border-medinfo-primary-main bg-white lg:size-[40px]"
 					>
 						<CameraIcon className="size-[16px] lg:size-[26px]" />
 					</div>
@@ -54,27 +54,27 @@ function ProfilePage() {
 				<Form.Root className="w-full max-w-[372px] gap-3 self-center" methods={userIdentityMethods}>
 					<Form.Field<typeof userIdentityMethods.control>
 						name="firstName"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">First Name</Form.Label>
 						<Form.Input
 							type="text"
-							className="border-medinfo-primary-main placeholder:text-medinfo-dark-4 h-[48px]
-								rounded-[8px] border-[1.4px] px-4 py-3 md:h-[64px] md:py-5 md:text-base"
+							className="h-[48px] rounded-[8px] border-[1.4px]
+								border-medinfo-primary-main px-4 py-3 placeholder:text-medinfo-dark-4 md:h-[64px] md:py-5 md:text-base"
 						/>
 					</Form.Field>
 					<Form.Field<typeof userIdentityMethods.control>
 						name="lastName"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">Last Name</Form.Label>
 						<Form.Input
 							type="text"
-							className="border-medinfo-primary-main placeholder:text-medinfo-dark-4 h-[48px]
-								rounded-[8px] border-[1.4px] px-4 py-3 md:h-[64px] md:py-5 md:text-base"
+							className="h-[48px] rounded-[8px] border-[1.4px]
+								border-medinfo-primary-main px-4 py-3 placeholder:text-medinfo-dark-4 md:h-[64px] md:py-5 md:text-base"
 						/>
 					</Form.Field>
-					<Form.Field name="gender" className="font-roboto gap-1 font-medium">
+					<Form.Field name="gender" className="gap-1 font-roboto font-medium">
 						<Form.Label className="md:text-[20px]">Gender</Form.Label>
 
 						<Form.FieldController
@@ -101,17 +101,17 @@ function ProfilePage() {
 									>
 										<Select.Item
 											value="male"
-											className="bg-medinfo-light-3 text-medinfo-dark-4 focus:bg-medinfo-light-1
-												focus:text-medinfo-body-color data-[state=checked]:bg-medinfo-light-1
-												h-[48px] font-medium md:h-[64px] md:text-base"
+											className="h-[48px] bg-medinfo-light-3 font-medium
+												text-medinfo-dark-4 focus:bg-medinfo-light-1
+												focus:text-medinfo-body-color data-[state=checked]:bg-medinfo-light-1 md:h-[64px] md:text-base"
 										>
 											Male
 										</Select.Item>
 										<Select.Item
 											value="female"
-											className="bg-medinfo-light-3 text-medinfo-dark-4 focus:bg-medinfo-light-1
-												focus:text-medinfo-body-color data-[state=checked]:bg-medinfo-light-1
-												h-[48px] font-medium md:h-[64px] md:text-base"
+											className="h-[48px] bg-medinfo-light-3 font-medium
+												text-medinfo-dark-4 focus:bg-medinfo-light-1
+												focus:text-medinfo-body-color data-[state=checked]:bg-medinfo-light-1 md:h-[64px] md:text-base"
 										>
 											Female
 										</Select.Item>
@@ -123,14 +123,14 @@ function ProfilePage() {
 
 					<Form.Field<typeof userIdentityMethods.control>
 						name="bio"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">Bio</Form.Label>
 
 						<Form.Input
 							type="textarea"
-							className="border-medinfo-primary-main placeholder:text-medinfo-dark-4 h-[163px]
-								rounded-[8px] border-[1.4px] px-4 py-5 md:h-[159px] md:text-base"
+							className="h-[163px] rounded-[8px] border-[1.4px]
+								border-medinfo-primary-main px-4 py-5 placeholder:text-medinfo-dark-4 md:h-[159px] md:text-base"
 						/>
 					</Form.Field>
 				</Form.Root>
@@ -150,24 +150,24 @@ function ProfilePage() {
 				<Form.Root className="w-full max-w-[372px] gap-3 self-center" methods={contactInfoMethods}>
 					<Form.Field<typeof contactInfoMethods.control>
 						name="email"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">Email</Form.Label>
 						<Form.Input
 							type="text"
-							className="border-medinfo-primary-main placeholder:text-medinfo-dark-4 h-[48px]
-								rounded-[8px] border-[1.4px] px-4 py-3 md:h-[64px] md:py-5 md:text-base"
+							className="h-[48px] rounded-[8px] border-[1.4px]
+								border-medinfo-primary-main px-4 py-3 placeholder:text-medinfo-dark-4 md:h-[64px] md:py-5 md:text-base"
 						/>
 					</Form.Field>
 					<Form.Field<typeof contactInfoMethods.control>
 						name="phoneNumber"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">Phone number</Form.Label>
 						<Form.Input
 							type="number"
-							className="border-medinfo-primary-main placeholder:text-medinfo-dark-4 h-[48px]
-								rounded-[8px] border-[1.4px] px-4 py-3 md:h-[64px] md:py-5 md:text-base"
+							className="h-[48px] rounded-[8px] border-[1.4px]
+								border-medinfo-primary-main px-4 py-3 placeholder:text-medinfo-dark-4 md:h-[64px] md:py-5 md:text-base"
 						/>
 					</Form.Field>
 				</Form.Root>
@@ -187,24 +187,24 @@ function ProfilePage() {
 				<Form.Root className="w-full max-w-[372px] gap-3 self-center" methods={locationMethods}>
 					<Form.Field<typeof locationMethods.control>
 						name="country"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">Country</Form.Label>
 						<Form.Input
 							type="text"
-							className="border-medinfo-primary-main placeholder:text-medinfo-dark-4 h-[48px]
-								rounded-[8px] border-[1.4px] px-4 py-3 md:h-[64px] md:py-5 md:text-base"
+							className="h-[48px] rounded-[8px] border-[1.4px]
+								border-medinfo-primary-main px-4 py-3 placeholder:text-medinfo-dark-4 md:h-[64px] md:py-5 md:text-base"
 						/>
 					</Form.Field>
 					<Form.Field<typeof locationMethods.control>
 						name="city"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">City</Form.Label>
 						<Form.Input
 							type="text"
-							className="border-medinfo-primary-main placeholder:text-medinfo-dark-4 h-[48px]
-								rounded-[8px] border-[1.4px] px-4 py-3 md:h-[64px] md:py-5 md:text-base"
+							className="h-[48px] rounded-[8px] border-[1.4px]
+								border-medinfo-primary-main px-4 py-3 placeholder:text-medinfo-dark-4 md:h-[64px] md:py-5 md:text-base"
 						/>
 					</Form.Field>
 				</Form.Root>
@@ -224,7 +224,7 @@ function ProfilePage() {
 				<Form.Root className="w-full max-w-[372px] gap-3 self-center" methods={changePasswordMethods}>
 					<Form.Field<typeof changePasswordMethods.control>
 						name="oldPassword"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">Old password</Form.Label>
 						<Form.Input
@@ -238,7 +238,7 @@ function ProfilePage() {
 					</Form.Field>
 					<Form.Field<typeof changePasswordMethods.control>
 						name="newPassword"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">New password</Form.Label>
 						<Form.Input
@@ -252,7 +252,7 @@ function ProfilePage() {
 					</Form.Field>
 					<Form.Field<typeof changePasswordMethods.control>
 						name="confirmPassword"
-						className="font-roboto gap-1 font-medium"
+						className="gap-1 font-roboto font-medium"
 					>
 						<Form.Label className="md:text-[20px]">Confirm password</Form.Label>
 						<Form.Input
--- apps/frontend/app/(dashboard)/patient/community/page.tsx
+++ apps/frontend/app/(dashboard)/patient/community/page.tsx
@@ -22,15 +22,15 @@ function Page() {
 			>
 				<div className="mt-[32px] space-y-[12px] lg:mt-0 lg:w-full lg:max-w-[661.8px]">
 					<div
-						className="border-medinfo-light-1 flex cursor-pointer flex-col gap-[16px] rounded-[8px]
-							border border-solid p-[16px] lg:flex-row lg:justify-between"
+						className="flex cursor-pointer flex-col gap-[16px] rounded-[8px] border
+							border-solid border-medinfo-light-1 p-[16px] lg:flex-row lg:justify-between"
 						onClick={() => handleNavigation("Malaria-parasites")}
 					>
 						<div className="flex gap-[12px]">
 							<Image src={MessageImage} alt="profile" className="size-[48px]" />
 							<div>
 								<h2 className="text-[20px]">Malaria parasites</h2>
-								<p className="text-medinfo-dark-3 font-normal">
+								<p className="font-normal text-medinfo-dark-3">
 									Lorem ipsum dolor sit amet consectetur. Eget vivamus.
 								</p>
 							</div>
@@ -38,14 +38,14 @@ function Page() {
 						<div className="hidden flex-col space-y-[5.5px] lg:flex">
 							<div className="flex items-center justify-end gap-[4px]">
 								<ViewIcon />
-								<p className="text-medinfo-dark-3 text-[14px] font-normal">13 views</p>
+								<p className="text-[14px] font-normal text-medinfo-dark-3">13 views</p>
 							</div>
 							<p className="rounded-[24px] bg-[#06C270] p-[6px] text-[12px] font-normal text-white">
 								2 new comments
 							</p>
 							<div className="flex items-center gap-[4px]">
 								<CommentIcon />
-								<p className="text-medinfo-dark-3 text-[14px] font-normal">32 comments</p>
+								<p className="text-[14px] font-normal text-medinfo-dark-3">32 comments</p>
 							</div>
 						</div>
 						<div className="flex flex-col items-start space-y-[5.5px] lg:hidden">
@@ -55,26 +55,26 @@ function Page() {
 							<div className="flex items-center gap-[6px]">
 								<div className="flex items-center gap-[4px]">
 									<ViewIcon />
-									<p className="text-medinfo-dark-3 text-[14px] font-normal">13 views</p>
+									<p className="text-[14px] font-normal text-medinfo-dark-3">13 views</p>
 								</div>
 
 								<div className="flex items-center gap-[4px]">
 									<CommentIcon />
-									<p className="text-medinfo-dark-3 text-[14px] font-normal">32 comments</p>
+									<p className="text-[14px] font-normal text-medinfo-dark-3">32 comments</p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div
-						className="border-medinfo-light-1 flex cursor-pointer flex-col gap-[16px] rounded-[8px]
-							border border-solid p-[16px] lg:flex-row lg:justify-between"
+						className="flex cursor-pointer flex-col gap-[16px] rounded-[8px] border
+							border-solid border-medinfo-light-1 p-[16px] lg:flex-row lg:justify-between"
 						onClick={() => handleNavigation("Typhoid")}
 					>
 						<div className="flex gap-[12px]">
 							<Image src={MessageImage} alt="profile" className="size-[48px]" />
 							<div>
 								<h2 className="text-[20px]">Typhoid</h2>
-								<p className="text-medinfo-dark-3 font-normal">
+								<p className="font-normal text-medinfo-dark-3">
 									Lorem ipsum dolor sit amet consectetur. Eget vivamus.
 								</p>
 							</div>
@@ -82,14 +82,14 @@ function Page() {
 						<div className="hidden flex-col space-y-[5.5px] lg:flex">
 							<div className="flex items-center justify-end gap-[4px]">
 								<ViewIcon />
-								<p className="text-medinfo-dark-3 text-[14px] font-normal">13 views</p>
+								<p className="text-[14px] font-normal text-medinfo-dark-3">13 views</p>
 							</div>
 							<div className="rounded-[24px] bg-[#06C270] p-[6px] text-white">
 								<p className="text-[12px] font-normal text-white">2 new comments</p>
 							</div>
 							<div className="flex items-center gap-[4px]">
 								<CommentIcon />
-								<p className="text-medinfo-dark-3 text-[14px] font-normal">32 comments</p>
+								<p className="text-[14px] font-normal text-medinfo-dark-3">32 comments</p>
 							</div>
 						</div>
 						<div className="flex flex-col items-start space-y-[5.5px] lg:hidden">
@@ -99,18 +99,18 @@ function Page() {
 							<div className="flex items-center gap-[6px]">
 								<div className="flex items-center gap-[4px]">
 									<ViewIcon />
-									<p className="text-medinfo-dark-3 text-[14px] font-normal">13 views</p>
+									<p className="text-[14px] font-normal text-medinfo-dark-3">13 views</p>
 								</div>
 
 								<div className="flex items-center gap-[4px]">
 									<CommentIcon />
-									<p className="text-medinfo-dark-3 text-[14px] font-normal">32 comments</p>
+									<p className="text-[14px] font-normal text-medinfo-dark-3">32 comments</p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<p
-						className="text-medinfo-primary-main mt-[32px] text-right text-[16px] font-medium
+						className="mt-[32px] text-right text-[16px] font-medium text-medinfo-primary-main
 							lg:text-left lg:text-[20px]"
 					>
 						See more
